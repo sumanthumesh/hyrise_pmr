551b0d314587de6322e99c1bd2b7e5e0f79c7265
Sumanth Umesh <sumanthu@umich.edu>
2026-01-15 17:55:31 -0500

    Created memory pool manager to which we can add created std::pmr::memory resource objects for later retrieval

---
+++ src/lib/storage/mem_pool.hpp
@@ -0,0 +1,59 @@
+#include <numa.h>
+#include <memory_resource>
+#include <vector>
+#include <iostream>
+#include <cassert>
+#include <unordered_map>
+#include <memory>
+
+
+void* allocate_on_numa_node(std::size_t bytes, int node);
+
+// Simple RAII wrapper for a NUMA-backed monotonic_buffer_resource
+class NumaMonotonicResource : public std::pmr::memory_resource {
+public:
+    NumaMonotonicResource(std::size_t size_bytes, int numa_node,
+                          std::pmr::memory_resource* upstream = std::pmr::null_memory_resource())
+    : _size(size_bytes),
+      _buffer(allocate_on_numa_node(size_bytes, numa_node)),
+    //   _upstream(upstream),
+      _mono(_buffer, _size, upstream)
+    {}
+
+    ~NumaMonotonicResource() override {
+        // Important: free NUMA memory
+        if (_buffer && _size) {
+            numa_free(_buffer, _size);
+        }
+    }
+
+protected:
+    void* do_allocate(std::size_t bytes, std::size_t alignment) override {
+        return _mono.allocate(bytes, alignment);
+    }
+
+    void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override {
+        _mono.deallocate(p, bytes, alignment);
+    }
+
+    bool do_is_equal(const std::pmr::memory_resource& other) const noexcept override {
+        return this == &other;
+    }
+
+private:
+    std::size_t _size{};
+    void* _buffer{};
+    // std::pmr::memory_resource* _upstream;    
+    std::pmr::monotonic_buffer_resource _mono;
+};
+
+namespace hyrise{
+class MemPoolManager
+{
+    public:
+    void create_pool(const std::string &pool_name,uint64_t size,int numa_node);
+    std::shared_ptr<NumaMonotonicResource> get_pool(const std::string &pool_name);
+    private:
+    std::unordered_map<std::string,std::shared_ptr<NumaMonotonicResource>> _pools;
+};
+}
