88209bf36dc551e1c84f12893e5bea195e380031
Sumanth Umesh <sumanthu@umich.edu>
2026-02-06 14:23:14 -0500

    Replaced the fmt::format with regular string due to some compiler incompatibility issues

--- src/lib/memory/mem_common.hpp
+++ src/lib/memory/mem_common.hpp
@@ -2,14 +2,15 @@
 
 #include <cstddef>
 #include <string>
-#include <fmt/format.h>
+#include <sstream>
+#include <iomanip>
 
 namespace hyrise
 {
 struct MemResourceStatus
 {
     std::string description{""};
-    size_t resource_id{0};
+    size_t resource_id{std::numeric_limits<size_t>::max()};
     size_t capacity_bytes{0};
     size_t allocated_bytes{0};
     size_t peak_allocated_bytes{0};
@@ -17,9 +18,11 @@ struct MemResourceStatus
 
     std::string to_string() const
     {
-        return "MemResourceStatus {" + description + ","+std::to_string(resource_id)+
-               ","+fmt::format("{:x}", capacity_bytes)+","+fmt::format("{:x}", allocated_bytes)+
-               ","+fmt::format("{:x}", peak_allocated_bytes)+","+std::to_string(numa_node)+"}";
+        std::stringstream ss;
+        ss << "MemResourceStatus {" << description << "," << std::dec << resource_id << ","
+           << std::dec << capacity_bytes << "," << std::dec << allocated_bytes << ","
+           << std::dec << peak_allocated_bytes << "," << std::dec << numa_node << "}";
+        return ss.str();
     }
 };
 } // namespace hyrise
