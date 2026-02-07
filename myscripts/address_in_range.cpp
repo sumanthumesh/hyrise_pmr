b725c688a60645806db5ad00083004113201ebe2
Sumanth Umesh <sumanthu@umich.edu>
2026-02-07 15:39:32 -0500

    Print success message

--- myscripts/address_in_range.cpp
+++ myscripts/address_in_range.cpp
@@ -82,6 +82,7 @@ int main(int argc,char **argv) {
             exit(EXIT_FAILURE);
         }
     }
+    std::cout<< "All addresses are within the specified ranges." << std::endl;
     
     return 0;
 }
