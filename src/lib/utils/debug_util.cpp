0f0e38abd5b0f0117e9f9d354c750ff7f871ac38
Sumanth Umesh <sumanthu@umich.edu>
2026-06-06 15:59:10 -0400

    Using boost stacktrace instead of the custom stacktrace

--- src/lib/utils/debug_util.cpp
+++ src/lib/utils/debug_util.cpp
@@ -1,31 +1,10 @@
 #include "debug_util.hpp"
 
+#include <boost/stacktrace.hpp>
+
 std::string print_backtrace()
 {
-    constexpr int max_frames = 64;
-    void *frames[max_frames];
-
-    // Capture the backtrace
-    int num_frames = backtrace(frames, max_frames);
-
-    // Convert addresses to symbols
-    char **symbols = backtrace_symbols(frames, num_frames);
-
-    if (symbols == nullptr)
-    {
-        std::cerr << "Failed to get backtrace symbols\n";
-        return "Failed to get backtrace symbols\n";
-    }
-
     std::ostringstream oss;
-
-    oss << "Backtrace (" << num_frames << " frames):\n";
-    for (int i = 0; i < num_frames; ++i)
-    {
-        oss << "  [" << i << "] " << symbols[i] << "\n";
-    }
-
-    free(symbols);
-
+    oss << "Backtrace:\n" << boost::stacktrace::stacktrace();
     return oss.str();
 }
