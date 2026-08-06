cd763bae5a2f9bf4d09bbf64c04b886a4d8d79ca
Sumanth <sumanthu@umich.edu>
2026-08-06 20:17:04 +0000

    These are the scripts I used to generate the current results

---
+++ myscripts/mapping/sf50_scripts/sf50_migration_d2c.sql
@@ -0,0 +1,45 @@
+setting print off
+setting binary_caching on
+setting workers 16
+generate_tpch 50
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+setting print_migration_stats off
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_local.sql
+setting print_migration_stats on
+script /home/umeshsum/hyrise_pmr/myscripts/all_cols_to_remote.sql
+quit
