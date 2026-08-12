c9bf0b538c6335179bdee9a10f32ee013c9fc562
Sumanth <sumanthu@umich.edu>
2026-08-12 01:58:03 +0000

    This script can now accept multiple query files. It also outputs the datatype of each column used

--- myscripts/cols_in_tpch_query.py
+++ myscripts/cols_in_tpch_query.py
@@ -1,7 +1,8 @@
 import os
 import sys
+import argparse
 
-from tpch_common import tpch_table_column_names
+from tpch_common import tpch_table_column_names, tpch_column_type
 
 def cols_in_query(filename):
     tpch_cols = [s[1] for s in tpch_table_column_names]
@@ -13,8 +14,24 @@ def cols_in_query(filename):
     return cols_in_query
 
 if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description="Extract column names used in a TPC-H query file")
+    parser.add_argument("query_files", nargs="+", help="Paths to the TPC-H query files")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    
+    all_cols = set()
+    
+    for query_file in args.query_files:
+        cols = cols_in_query(query_file)
+        all_cols.update(cols)
 
-    query_file = sys.argv[1]
-    cols = cols_in_query(query_file)
-    for c in cols:
-        print(c)
+        if args.verbose:
+            print(f"Columns in {query_file}:")
+            for c in cols:
+                print(c)
+                
+    print(f"Columns used across {len(args.query_files)} queries:")
+    table_name_from_column = {col: table for table, col in tpch_table_column_names}
+    for c in all_cols:
+        print(f"{c},{tpch_column_type[(table_name_from_column[c], c)]}")
