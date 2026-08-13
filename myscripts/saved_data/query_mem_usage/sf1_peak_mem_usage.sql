64505228b185b08eebee612f36e30b312d067953
Sumanth Umesh <sumanthu@umich.edu>
2026-08-13 17:30:28 -0400

    These files show mem usage by different queries and different scaling factors

---
+++ myscripts/saved_data/query_mem_usage/sf1_peak_mem_usage.sql
@@ -0,0 +1,35 @@
+setting print off
+setting binary_caching on
+setting workers 16
+hsh mem_usage
+generate_tpch 1
+hsh mem_usage
+hsh new_mem 64000000000 1 3
+hsh mem_usage
+setting workers 8
+setting mem_strategy Remote
+setting track_peak on
+hsh mem_usage
+script /data1/sumanthu/tpch_queries/1.sql
+script /data1/sumanthu/tpch_queries/2.sql
+script /data1/sumanthu/tpch_queries/3.sql
+script /data1/sumanthu/tpch_queries/4.sql
+script /data1/sumanthu/tpch_queries/5.sql
+script /data1/sumanthu/tpch_queries/6.sql
+script /data1/sumanthu/tpch_queries/7.sql
+script /data1/sumanthu/tpch_queries/8.sql
+script /data1/sumanthu/tpch_queries/9.sql
+script /data1/sumanthu/tpch_queries/10.sql
+script /data1/sumanthu/tpch_queries/11.sql
+script /data1/sumanthu/tpch_queries/12.sql
+script /data1/sumanthu/tpch_queries/13.sql
+script /data1/sumanthu/tpch_queries/14.sql
+script /data1/sumanthu/tpch_queries/16.sql
+script /data1/sumanthu/tpch_queries/17.sql
+script /data1/sumanthu/tpch_queries/18.sql
+script /data1/sumanthu/tpch_queries/19.sql
+script /data1/sumanthu/tpch_queries/20.sql
+script /data1/sumanthu/tpch_queries/21.sql
+script /data1/sumanthu/tpch_queries/22.sql
+hsh mem_usage
+quit
