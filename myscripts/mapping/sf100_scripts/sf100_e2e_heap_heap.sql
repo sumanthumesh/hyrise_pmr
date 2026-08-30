0259555e28510bb97cad3fe4d7cea1df86bdf6cb
Sumanth <sumanthu@umich.edu>
2026-08-30 03:32:04 +0000

    This script will run all queries with full DRAM capacity. The idea is that this will present full DRAM capacity alongside DAM+SSD and DAM+CRAM runs for motivation

---
+++ myscripts/mapping/sf100_scripts/sf100_e2e_heap_heap.sql
@@ -0,0 +1,237 @@
+setting print off
+setting binary_caching on
+setting workers 16
+generate_tpch 100
+setting workers 8
+setting label d0_tf0_q10
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+script /home/umeshsum/tpch_queries/10.sql
+setting label d0_tf0_q11
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+script /home/umeshsum/tpch_queries/11.sql
+setting label d0_tf0_q12
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+script /home/umeshsum/tpch_queries/12.sql
+setting label d0_tf0_q13
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+script /home/umeshsum/tpch_queries/13.sql
+setting label d0_tf0_q14
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+script /home/umeshsum/tpch_queries/14.sql
+setting label d0_tf0_q16
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+script /home/umeshsum/tpch_queries/16.sql
+setting label d0_tf0_q17
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+script /home/umeshsum/tpch_queries/17.sql
+setting label d0_tf0_q18
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+script /home/umeshsum/tpch_queries/18.sql
+setting label d0_tf0_q19
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+script /home/umeshsum/tpch_queries/19.sql
+setting label d0_tf0_q1
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+script /home/umeshsum/tpch_queries/1.sql
+setting label d0_tf0_q20
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+script /home/umeshsum/tpch_queries/20.sql
+setting label d0_tf0_q21
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+script /home/umeshsum/tpch_queries/21.sql
+setting label d0_tf0_q22
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+script /home/umeshsum/tpch_queries/22.sql
+setting label d0_tf0_q2
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+script /home/umeshsum/tpch_queries/2.sql
+setting label d0_tf0_q3
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+script /home/umeshsum/tpch_queries/3.sql
+setting label d0_tf0_q4
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+script /home/umeshsum/tpch_queries/4.sql
+setting label d0_tf0_q5
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+script /home/umeshsum/tpch_queries/5.sql
+setting label d0_tf0_q6
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+script /home/umeshsum/tpch_queries/6.sql
+setting label d0_tf0_q7
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+script /home/umeshsum/tpch_queries/7.sql
+setting label d0_tf0_q8
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+script /home/umeshsum/tpch_queries/8.sql
+setting label d0_tf0_q9
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+script /home/umeshsum/tpch_queries/9.sql
+quit
