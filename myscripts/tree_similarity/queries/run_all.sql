77e04ff85a446033ac2caeecb2bd125b1d80565a
Sumanth Umesh <sumanthu@umich.edu>
2026-08-31 01:22:45 -0400

    Run all tree similarity queries

---
+++ myscripts/tree_similarity/queries/run_all.sql
@@ -0,0 +1,20 @@
+setting print off
+setting workers 16
+generate_tpch 1
+setting workers 8
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/091.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/092.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/093.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/094.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/095.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/131.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/132.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/133.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/134.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/135.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/211.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/212.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/213.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/214.sql
+script /home/umeshsum/hyrise_pmr/myscripts/tree_similarity/queries/215.sql
+quit
